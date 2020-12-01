7e828e099d4b022595b0cb57ace6dd454763728e
Scotsoo <jack.scotson@netacea.com>
2020-12-01 14:31:35 +0000

    Testing echos

--- services/DummyService/index.js
+++ services/DummyService/index.js
@@ -1,5 +1,6 @@
 const dummyModule = require('@scotsoo/dummymodule')
 const secondDummyModule = require('@scotsoo/seconddummymodule')
+console.log('inside dummy service!')
 module.exports = {
   dummyModule,
   secondDummyModule
--- services/SecondDummyService/index.js
+++ services/SecondDummyService/index.js
@@ -1,5 +1,6 @@
 const dummyModule = require('@scotsoo/dummymodule')
 const secondDummyModule = require('@scotsoo/seconddummymodule')
+console.log('inside second dummy service')
 module.exports = {
   dummyModule,
   secondDummyModule
